7e23cd7787e7a4fa6ca0ee166bd1ac210bd3d26a
Bryan Weber <bryan@cobenian.com>
2015-08-17 08:55:33 -0400

    removing call that was causing problems hopefully, will have to investigate further

--- lib/raygun/format.ex
+++ lib/raygun/format.ex
@@ -155,7 +155,7 @@ defmodule Raygun.Format do
         httpMethod: conn.method,
         iPAddress: conn.remote_ip |> :inet.ntoa |> List.to_string,
         queryString: Plug.Conn.fetch_query_params(conn),
-        form: Plug.Parsers.call(conn, []).params,
+        form: %{}, # todo fix me!!! Plug.Parsers.call(conn, []).params,
         headers: Raygun.Util.format_headers(conn.req_headers),
         rawData: %{}
       }
