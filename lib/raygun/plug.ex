76ec8df1722292d0fededf83a6cc5e8d48d2edd5
Bryan Weber <bryan@cobenian.com>
2015-08-18 09:19:20 -0400

    trying to figure out plug config state

--- lib/raygun/plug.ex
+++ lib/raygun/plug.ex
@@ -4,7 +4,10 @@ defmodule Raygun.Plug do
   any exceptions will be sent to Raygun.
   """
 
-  defmacro __using__(_env) do
+  @user_fn nil
+
+  defmacro __using__(env) do
+    @user_fn = env.user
     quote location: :keep do
       @before_compile Raygun.Plug
     end
@@ -13,27 +16,17 @@ defmodule Raygun.Plug do
   @doc """
   Whenever an error occurs, capture the stacktrace and exception to send to Raygun.
   """
-  defmacro __before_compile__(env) do
+  defmacro __before_compile__(_env) do
     quote location: :keep do
       defoverridable [call: 2]
 
       def call(conn, opts) do
-        IO.puts "opts are:"
-        IO.inspect opts
-        IO.puts "env is:"
-        ev = unquote(env)
-        IO.inspect ev
         try do
           super(conn, opts)
         rescue
           exception ->
             stacktrace = System.stacktrace
-            IO.inspect opts
-            if Keyword.has_key?(ev, :user) do
-              user = ev.user.(conn)
-            else
-              user = nil
-            end
+            user = if @user_fn, do: @user_fn.(conn)
             IO.puts "user is #{user}"
             Raygun.report_plug(conn, stacktrace, exception, env: Atom.to_string(Mix.env), user: user)
             reraise exception, stacktrace
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Raygun.Mixfile do
 
   def project do
     [app: :raygun,
-     version: "0.0.27",
+     version: "0.0.28",
      elixir: "~> 1.0",
      source_url: "https://github.com/cobenian/raygun",
      build_embedded: Mix.env == :prod,
