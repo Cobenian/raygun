a1bbdb46882c2274775f877bdcb87672ada7730a
Bryan Weber <bryan@cobenian.com>
2015-08-18 16:29:13 -0400

    adding updated docs

--- doc/dist/sidebar_items.js
+++ doc/dist/sidebar_items.js
@@ -3,11 +3,13 @@ sidebarNodes = {
     {"id": "Raygun",
 "docs": ["report_exception/2","report_message/2","report_plug/4","report_stacktrace/3","send_report/1","send_report_to_raygun/1"]
 },{"id": "Raygun.Format",
-"docs": ["conn_payload/4","custom/1","details/0","environment/0","err/2","message_payload/2","now/0","request/1","response/1","stacktrace/1","stacktrace_entry/1","stacktrace_payload/3","user/0","user/2"]
+"docs": ["conn_payload/4","custom/1","details/0","environment/0","err/2","message_payload/2","now/0","request/1","response/1","stacktrace/1","stacktrace_entry/1","stacktrace_payload/3","user/1"]
 },{"id": "Raygun.Logger",
 "docs": ["handle_call/2","handle_event/2"]
 },{"id": "Raygun.Plug",
-"docs": ["__before_compile__/1"]
+"docs": ["__before_compile__/1","get_user/2"]
+},{"id": "Raygun.Plug.State",
+"docs": ["get/0","set/1","start_link/0"]
 },{"id": "Raygun.Util",
 "docs": ["file_from/1","format_headers/1","function_and_arity/2","line_from/1","mod_for/1"]
 
