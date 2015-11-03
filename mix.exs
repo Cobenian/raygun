f1a06bea34b4f9f69910072844617a287c85be31
Aaron Jensen <aaronjensen@gmail.com>
2015-11-03 12:04:49 -0800

    Relax poison dependency

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule Raygun.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:poison, "~> 1.4.0"},
+      {:poison, "~> 1.4"},
       {:httpoison, "~> 0.7.2"},
       {:timex, "~> 0.19.4"},
       {:plug, "~> 1.0.0"},
