67c858dad1063d33c01cfb1fe7cbba95aeebf9cb
Eduardo Gurgel <eduardo@carnivallabs.com>
2016-01-28 10:40:08 +1300

    Refactor Raygun module
    Also add tests to report_stacktrace

--- test/raygun_test.exs
+++ test/raygun_test.exs
@@ -1,7 +1,24 @@
 defmodule RaygunTest do
   use ExUnit.Case
+  import :meck
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  defmodule MyAppError do
+    defexception [:message]
+  end
+
+  setup do
+    new [HTTPoison, Poison]
+    on_exit fn -> unload end
+    :ok
+  end
+
+  test "report_stacktrace with successful response" do
+    response = %HTTPoison.Response{status_code: 202}
+
+    expect(Raygun.Format, :stacktrace_payload, [:stacktrace, :error, []], :payload)
+    expect(Poison, :encode!, [:payload], :json)
+    expect(HTTPoison, :post, ["https://api.raygun.io/entries", :json, :_], {:ok, response})
+
+    assert Raygun.report_stacktrace(:stacktrace, :error) == :ok
   end
 end
