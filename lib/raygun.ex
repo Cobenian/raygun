588cb867a23b152dc62774e9823a4c8338eacbe0
James <tslcjames@gmail.com>
2016-10-25 15:57:46 +0700

    add option for HTTPoison usage

--- lib/raygun.ex
+++ lib/raygun.ex
@@ -52,7 +52,8 @@ defmodule Raygun do
       "User-Agent": "Elixir Client",
       "X-ApiKey": Raygun.Util.get_env(:raygun, :api_key)
     }
-    case HTTPoison.post(@api_endpoint, Poison.encode!(error), headers) do
+    opts = Application.get_env(:raygun, :httpoison_opts, [])
+    case HTTPoison.post(@api_endpoint, Poison.encode!(error), headers, opts) do
       { :ok, %HTTPoison.Response{status_code: 202} } -> :ok
       { :ok, %HTTPoison.Response{status_code: 400} } -> {:error, :bad_message}
       { :ok, %HTTPoison.Response{status_code: 403} } -> {:error, :invalid_api_key}
