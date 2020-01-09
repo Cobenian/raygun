4f5e8c7b2b926446aa764bc7fc8e27a469ce4759
Thomas Cioppettini <544875+tomciopp@users.noreply.github.com>
2020-01-09 11:42:15 -0500

    chore(Dependencies): Update deps to most recent versions, and replace Poison with Jason

--- mix.exs
+++ mix.exs
@@ -2,24 +2,26 @@ defmodule Raygun.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :raygun,
-     version: "0.3.2",
-     elixir: "~> 1.3",
-     source_url: "https://github.com/cobenian/raygun",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     description: description,
-     package: package,
-     test_coverage: [tool: ExCoveralls],
-     preferred_cli_env: preferred_cli_env,
-     deps: deps]
+    [
+      app: :raygun,
+      version: "0.3.2",
+      elixir: "~> 1.3",
+      source_url: "https://github.com/cobenian/raygun",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: preferred_cli_env(),
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger, :httpoison, :plug, :poison]]
+    [applications: [:logger, :httpoison, :plug]]
   end
 
   defp description do
@@ -33,7 +35,7 @@ defmodule Raygun.Mixfile do
 
   defp preferred_cli_env do
     [
-      "coveralls": :test,
+      coveralls: :test,
       "coveralls.detail": :test,
       "coveralls.post": :test,
       "coveralls.html": :test
@@ -51,20 +53,22 @@ defmodule Raygun.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:poison, "~> 2.0 or ~> 1.0"},
-      {:httpoison, "~> 0.8"},
+      {:jason, "~> 1.1"},
+      {:httpoison, "~> 1.6.2"},
       {:plug, "~> 1.1"},
-      {:earmark, "~> 0.2", only: :dev},
-      {:ex_doc, "~> 0.11", only: :dev},
-      {:meck, "~> 0.8", only: :test},
-      {:excoveralls, "~> 0.5.4", only: :test}
+      {:earmark, "~> 1.4.3", only: :dev},
+      {:ex_doc, "~> 0.21.2", only: :dev},
+      {:meck, "~> 0.8.13", only: :test},
+      {:excoveralls, "~> 0.12.1", only: :test}
     ]
   end
 
   defp package do
-    [# These are the default files included in the package
-     maintainers: ["Bryan Weber"],
-     licenses: ["Apache 2.0"],
-     links: %{"GitHub" => "https://github.com/cobenian/raygun"}]
+    # These are the default files included in the package
+    [
+      maintainers: ["Bryan Weber"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/cobenian/raygun"}
+    ]
   end
 end
